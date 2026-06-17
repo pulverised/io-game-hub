395ea118995c09ca5652f42c3724a6c9a30195f3
pulverised <134840828+pulverised@users.noreply.github.com>
2026-06-17 11:26:24 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,26 +1,22 @@
 const game =
 JSON.parse(
-localStorage.getItem("selectedGame")
+localStorage.getItem(
+"selectedGame"
+)
 );
 
 
 
-const iframe =
+const frame =
 document.getElementById(
 "game-frame"
 );
 
 
+
 const title =
 document.getElementById(
 "title"
-);
-
-
-
-const overlay =
-document.getElementById(
-"mouse-overlay"
 );
 
 
@@ -32,79 +28,9 @@ title.textContent =
 game.title;
 
 
-iframe.src =
+
+frame.src =
 game.url;
 
 
 }
-
-
-
-
-
-// Click to lock mouse
-
-overlay.addEventListener(
-"click",
-()=>{
-
-
-    iframe.focus();
-
-
-    // Request pointer lock
-
-    document.body.requestPointerLock();
-
-
-    overlay.style.display="none";
-
-
-});
-
-
-
-
-
-// Detect mouse unlock
-
-document.addEventListener(
-"pointerlockchange",
-()=>{
-
-
-if(
-document.pointerLockElement === null
-)
-
-{
-
-
-overlay.style.display="flex";
-
-
-}
-
-
-});
-
-
-
-
-
-// Press ESC unlocks mouse normally
-
-document.addEventListener(
-"keydown",
-(event)=>{
-
-
-if(event.key==="Escape")
-{
-
-overlay.style.display="flex";
-
-}
-
-
-});
